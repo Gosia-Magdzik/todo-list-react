f065af63d7be227db17165d343da6d7397530aea
Gosia Magdzik <melassa26@gmail.com>
2023-01-18 21:25:59 +0100

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Form from "./form";
-import Tasks from "./Tasks";
+import Form from './form';
+import Tasks from './Tasks';
 import Buttons from './Buttons';
 import Section from './Section';
+import Header from './Header';
 
 const tasks = [
   {id: 1, content: "przejść na reacta", done: false},
@@ -13,17 +14,14 @@ const hideDoneTasks = false;
 function App() {
   return (
 <main className="body">
-    <h1>Lista Zadań</h1>
+    <Header title="Lista zadań"/>
     <Section  title="Dodaj nowe zadanie"
               body={<Form/>} 
     />
-
     <Section  title="Lista zadań"
               body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />} 
               extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>}
     />
-
-
 </main>
   );
 }
