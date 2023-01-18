5cc015f68a10f394862abd371ec7bcc3c9b171c2
Gosia Magdzik <melassa26@gmail.com>
2023-01-18 11:01:40 +0100

    tasks => react

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,8 +3,18 @@ import "./style.css";
 const Tasks = (props) => (
     <ul className="list">
         {props.tasks.map(task => (
-            <li key={task.id}>
-                {task.content}
+            <li key={task.id}
+            className={`list__items  {task.done && props.hideDoneTasks ? "list__items--hidden" : "list__items"}`}
+            >
+                <button  className="list__button list__button--done">
+                         {task.done ? "✔" : ""}
+                    </button>
+                        <span className={`list__task {task.done ? "list__task--done" : ""}`}>
+                         {task.content}
+                        </span>
+                    <button  className="list__button list__button--remove">
+                        🗑
+                    </button>
             </li>
         ))}
     </ul> 
