33e00e7944885973ffd15a454412af1f9c13256e
Gosia Magdzik <melassa26@gmail.com>
2023-01-19 22:55:11 +0100

    last commit - formatting

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,20 +1,24 @@
 import "./style.css";
 
 const Buttons = ({tasks, hideDoneTasks}) => (
-    tasks.length > 0 && (
-    <div className="Buttons">
-        <button className= "hideShow">
-            {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-        </button>
-        <button className="finishAll"
-            disabled={tasks.every(({ done }) => done)}
-        >
-                Ukończ wszystkie
-        </button>
-    </div>
-)
-);
     
-
+    <div className="Buttons">
+        {tasks.length > 0 && (
+        <>
+            <button 
+                className= "hideShow">
+                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+            </button>
+            <button 
+                className="finishAll"
+                autoFocus
+                disabled={tasks.every(({ done }) => done)}
+            >
+                Ukończ wszystkie
+            </button>
+        </>
+    )}
+    </div>
+); 
 
 export default Buttons;
