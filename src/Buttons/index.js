423c88e67cfefef4d3ec86650967f07b6e73dbf7
Gosia Magdzik <melassa26@gmail.com>
2023-01-20 13:54:36 +0100

    lowercase to the className

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Buttons = ({tasks, hideDoneTasks}) => (
     
-    <div className="Buttons">
+    <div className="buttons">
         {tasks.length > 0 && (
         <>
             <button 
