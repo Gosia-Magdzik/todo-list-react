b871da034a97e4a49afbda858c1c7f9f98f6a8be
Gosia Magdzik <melassa26@gmail.com>
2023-02-06 14:11:51 +0100

    + function getInitialTasks to increase efficiency

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Section from './Section';
 import Header from './Header';
 import Container from './Container';
 
+const getInitialTasks = () => {
+      const tasksFromLocalStorage = localStorage.getItem("tasks");
+      
+      return tasksFromLocalStorage
+             ? JSON.parse(tasksFromLocalStorage)
+             : [];
+};
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
   
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-  
-  const [tasks, setTasks] = useState(
-     tasksFromLocalStorage
-     ? JSON.parse(tasksFromLocalStorage)
-     : []
-  );
+  const [tasks, setTasks] = useState(getInitialTasks);
 
-useEffect (() => {
+  useEffect (() => {
       localStorage.setItem("tasks", JSON.stringify(tasks));
-}, [tasks]);
+      }, [tasks]);
 
   const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone);
