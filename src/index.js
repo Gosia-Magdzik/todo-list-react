4f4faef7f4127146e6f86079a255e417cdc33df0
Gosia Magdzik <melassa26@gmail.com>
2023-02-08 14:21:43 +0100

    + theme provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { theme } from "./theme";
+import { themeProvider } from 'styled-components';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <themeProvider theme={theme}>
+      <App />
+    </themeProvider>
   </React.StrictMode>
 );
 
