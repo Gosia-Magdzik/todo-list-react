0e73a21288675eb92d7a679bccbafce0abb83526
Gosia Magdzik <melassa26@gmail.com>
2023-02-09 15:19:20 +0100

    App component recreated

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React, { useState, useEffect } from 'react';
 import Form from './Form';
 import Tasks from './Tasks';
 import Buttons from './Buttons';
 import Section from './Section';
 import Header from './Header';
 import Container from './Container';
-
-const getInitialTasks = () =>
-      JSON.parse(localStorage.getItem("tasks")) || [];
-
+import { useTasks } from './useTasks'
 
 function App() {
-      const [hideDone, setHideDone] = useState(false);
+      const {
+            tasks,
+            hideDone,
+            removeTask,
+            toggleTaskDone,
+            addNewTask,
+            setAllDone,
+            toggleHideDone,
+      } = useTasks();
 
-      const [tasks, setTasks] = useState(getInitialTasks);
-
-      useEffect(() => {
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-      }, [tasks]);
-
-      const toggleHideDone = () => {
-            setHideDone(hideDone => !hideDone);
-      };
-
-      const removeTask = (id) => {
-            setTasks(tasks => tasks.filter(task => task.id !== id));
-      };
-
-      const toggleTaskDone = (id) => {
-            setTasks(tasks => tasks.map(task => {
-                  if (task.id === id) {
-                        return { ...task, done: !task.done };
-                  }
-                  return task;
-            }));
-      };
-
-      const setAllDone = () => {
-            setTasks(tasks => tasks.map(task => ({
-                  ...task,
-                  done: true,
-            })));
-      };
-
-      const addNewTask = (content) => {
-            setTasks(tasks => [
-                  ...tasks,
-                  {
-                        content,
-                        done: false,
-                        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-                  },
-            ]);
-      };
 
       return (
             <Container>
@@ -74,6 +38,6 @@ function App() {
                   />
             </Container>
       );
-}
+};
 
 export default App;
