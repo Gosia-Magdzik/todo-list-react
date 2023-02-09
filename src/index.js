a55e145c661eeb4bb0517a27a8c7ee4ff3843754
Gosia Magdzik <melassa26@gmail.com>
2023-02-09 15:50:26 +0100

    Capital letter to the theme provider

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { themeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <themeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <App />
       <GlobalStyle/>
-    </themeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
