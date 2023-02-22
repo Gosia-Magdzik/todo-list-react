4ec92132b933f6df34eb9d514ccdbb2e0639a017
Gosia Magdzik <melassa26@gmail.com>
2023-02-22 15:03:43 +0100

    useSelector imported

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,28 +1,34 @@
 import { List, Item, Content, ToggleDoneButton, RemovedButton  } from "./styled";
+import { useSelector } from "react-redux";
 
-const TasksList = ({tasks, hideDone, removeTask, toggleTaskDone}) => (
-    <List>
+const TasksList = ({ removeTask, toggleTaskDone}) => {
+  const {tasks, hideDone} = useSelector(selectTasks);  
+  
+  return (
+      <List>
         {tasks.map(task => (
             <Item 
-                key={task.id}
-                hidden={task.done && hideDone}
+              key={task.id}
+              hidden={task.done && hideDone}
             >
-                <ToggleDoneButton
-                        onClick = {() => toggleTaskDone(task.id)}
-                >
-                        {task.done ? "✔" : ""}
-                </ToggleDoneButton>
-                        <Content done={task.done}>
-                            {task.id} - {task.content}
-                        </Content>
-                <RemovedButton
-                         onClick= {() => removeTask(task.id)}
-                >
-                        🗑
-                </RemovedButton>
+            <ToggleDoneButton
+               onClick = {() => toggleTaskDone(task.id)}
+            >
+                {task.done ? "✔" : ""}
+            </ToggleDoneButton>
+                <Content done=
+                          {task.done}>
+                          {task.id} - {task.content}
+                </Content>
+            <RemovedButton
+                onClick= {() => removeTask(task.id)}
+            >
+            🗑
+            </RemovedButton>
             </Item>
-        ))}
-    </List> 
-);
+                ))}
+        </List> 
+    );
+};
 
 export default TasksList;
