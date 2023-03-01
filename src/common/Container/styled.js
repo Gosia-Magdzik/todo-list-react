e1916cc2d1975e067de721d766ab6ab44db9fc9f
Gosia Magdzik <melassa26@gmail.com>
2023-03-01 18:15:07 +0100

    wider content

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const TaskContainer = styled.main`
-    max-width: 800px;
+    max-width: 900px;
     margin: 0 auto;
     padding: 20px;
     word-break: break-word;
