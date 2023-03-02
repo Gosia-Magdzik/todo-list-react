9b6c6529ed4e440d3c32fd175a941b6f69e93ecd
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-02 16:40:01 +0100

    App.js -> BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import  Tasks  from "./features/tasks/Tasks";
 
-export default () => <Tasks/>;
+export default () => (
+   <BrowserRouter>
+      <nav>
+         <ul>
+            <li>
+                <Link to="/zadania">Zadania</Link>
+            </li>
+         </ul>
+         <Switch>
+            <Route path="/zadania">
+               <Tasks/>
+            </Route>
+         </Switch>
+      </nav>
+   </BrowserRouter>
+);
