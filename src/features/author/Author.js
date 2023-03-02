5b8c2f25708bca06afc8c483b47738a843a611ae
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-02 17:38:56 +0100

    Author page styled

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -1,6 +1,7 @@
 import Section from '../../common/Section';
 import Header from '../../common/Header';
 import Container from '../../common/Container';
+import { Wrapper } from './styled';
 
 function Author () {
     return (
@@ -9,7 +10,19 @@ function Author () {
             title="O autorce" />
         <Section 
             title="Małgorzata Magdzik"
-            body={<>blablabla</>}
+            body={
+                <Wrapper>
+                    <p>
+                        <strong>Hi There!👋,</strong>
+                    </p>
+                    <p>🌸It's nice to see You here🌸</p>
+                    <p>   
+                        This application has been created as a project from YouCode curse for frontend Developers&nbsp;
+                        <a href="https://youcode.pl/zostawiam-maila/" target="_blank">YouCode</a><br/>
+                        I invite You too see more of my projects 🠲 <a href="https://github.com/Gosia-Magdzik" target="_blank">GitHub page</a>
+                    </p>
+                </Wrapper>
+            }
         />          
     </Container>
     );
