e5f529db2f89cf6664b6d2cc2dffc25648b4e57c
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-02 16:57:47 +0100

    Author page to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import  Tasks  from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
 export default () => (
    <BrowserRouter>
@@ -8,10 +9,16 @@ export default () => (
             <li>
                 <Link to="/zadania">Zadania</Link>
             </li>
+            <li>
+                <Link to="/ToAuthor">O autorce</Link>
+            </li>
          </ul>
          <Switch>
             <Route path="/zadania">
                <Tasks/>
+            </Route>
+            <Route path="/ToAuthor">
+               <Author/>
             </Route>
          </Switch>
       </nav>
