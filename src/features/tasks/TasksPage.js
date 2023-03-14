d33d518303976ccb4ebc267f061527d429f68823
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-14 18:06:43 +0100

    rename Tasks - TasksPage

---
+++ src/features/tasks/TasksPage.js
@@ -0,0 +1,30 @@
+import Form from './Form';
+import TasksList from './TasksList';
+import Buttons from './Buttons';
+import Section from '../../common/Section';
+import Header from '../../common/Header';
+import Container from '../../common/Container';
+
+
+function TasksPage() {   
+
+      return (
+            <Container>
+                  <Header title="Lista Zadań" />
+                  <Section title="Dodaj nowe zadanie"
+                        body={<Form/>}
+                  />
+                  <Section 
+                        title="Lista zadań"
+                        body={
+                              <TasksList />
+                        }
+                        extraHeaderContent={
+                              <Buttons/>
+                        }
+                  />
+            </Container>
+      );
+}
+
+export default TasksPage;
