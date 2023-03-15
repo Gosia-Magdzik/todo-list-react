85ba17b7d6eccca99a2c00a94bf4248c44d4f9cb
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-15 14:21:03 +0100

    Task content added

---
+++ src/features/tasks/taskPage/index.js
@@ -0,0 +1,18 @@
+import Section from '../../../common/Section';
+import Header from '../../../common/Header';
+import Container from '../../../common/Container';
+
+
+function TaskPage() {   
+      return (
+            <Container>
+                  <Header title="Szczegóły zadania" />
+                  <Section 
+                        title={""}
+                        body={<>blabalbal</>}
+                  />
+            </Container>
+      );
+}
+
+export default TaskPage;
