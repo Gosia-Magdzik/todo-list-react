64eb57cea46a2890ef56e3fa973ba9646222f9b6
Gosia Magdzik <120942733+Gosia-Magdzik@users.noreply.github.com>
2023-03-16 17:29:22 +0100

    useLocation Hook added

--- src/features/tasks/tasksPage/TasksList/index.js
+++ src/features/tasks/tasksPage/TasksList/index.js
@@ -1,12 +1,16 @@
 import { List, Item, Content, ToggleDoneButton, RemovedButton, StyledLink } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleTaskDone, removeTask, selectHideDone }  from "../../tasksSlice";
+import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery }  from "../../tasksSlice";
+import { useLocation } from "react-router-dom";
 
 const TasksList = () => {
-  const tasks = useSelector(selectTasks); 
-  const hideDone = useSelector(selectHideDone);  
+   const location = useLocation(); 
+   const query = (new URLSearchParams(location.search)).get("szukaj");
 
-  const dispatch = useDispatch();
+   const tasks = useSelector(state => selectTasksByQuery(state, query)); 
+   const hideDone = useSelector(selectHideDone);  
+
+   const dispatch = useDispatch();
   
     return (
         <List>
